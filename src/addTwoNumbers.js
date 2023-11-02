d54b66eef8c3784bea5443fb72fe187150422d45
justin.farrell <justin.farrell@lightcast.io>
2023-11-02 16:29:00 -0400

    addTwoNumbers created

---
+++ src/addTwoNumbers.js
@@ -0,0 +1,10 @@
+
+
+/**
+ * This is a function to add two numbers together!
+ * @param {number} num1 The first number to sum.
+ * @param {number} num2 The second number to sum.
+ * 
+ * @returns {number}
+*/
+export const addTwoNumbers = (num1, num2) => num1 + num2;
