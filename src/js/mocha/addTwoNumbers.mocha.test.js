2b05b13d3db3bd174a5176cc2ba8a6a00dcd18f6
justin.farrell <justin.farrell@lightcast.io>
2023-11-13 23:17:39 -0500

    Fixed weird syntax in Mocha test and added a JSX test file for Mocha

--- src/js/mocha/addTwoNumbers.mocha.test.js
+++ src/js/mocha/addTwoNumbers.mocha.test.js
@@ -1,4 +1,4 @@
-import mocha from "mocha";
+import { describe, it } from "mocha";
 import { addTwoNumbers } from "../addTwoNumbers.js";
 import { assert } from 'chai'
 
@@ -12,14 +12,14 @@ const TEST_RESULT1 = 4;
 const TEST_RESULT2 = 9.5;
 const TEST_RESULT3 = -1;
 
-mocha.describe('addTwoNumbers - mocha (with chai as an assertion library)', () => {
-    mocha.it(`should add ${TEST_NUM1} and ${TEST_NUM2} and return ${TEST_RESULT1}`, () => {
+describe('addTwoNumbers - mocha (with chai as an assertion library)', () => {
+    it(`should add ${TEST_NUM1} and ${TEST_NUM2} and return ${TEST_RESULT1}`, () => {
         assert(addTwoNumbers(TEST_NUM1, TEST_NUM2) === TEST_RESULT1, `${addTwoNumbers(TEST_NUM1, TEST_NUM2)} !== ${TEST_RESULT1}`);
     })
-    mocha.it(`should add ${TEST_NUM3} and ${TEST_NUM4} and return ${TEST_RESULT2}`, () => {
+    it(`should add ${TEST_NUM3} and ${TEST_NUM4} and return ${TEST_RESULT2}`, () => {
         assert(addTwoNumbers(TEST_NUM3, TEST_NUM4), `${addTwoNumbers(TEST_NUM3, TEST_NUM4)} !== ${TEST_RESULT2}`);
     })
-    mocha.it(`should add ${TEST_NUM1} and ${TEST_NUM5} and return ${TEST_RESULT3}`, () => {
+    it(`should add ${TEST_NUM1} and ${TEST_NUM5} and return ${TEST_RESULT3}`, () => {
         assert(addTwoNumbers(TEST_NUM1, TEST_NUM5), `${addTwoNumbers(TEST_NUM1, TEST_NUM5)} !== ${TEST_RESULT3}`);
     })
 })
